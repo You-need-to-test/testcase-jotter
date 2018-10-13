3d1e01a844ccd54f5df41bad7871370f2f63f82a
Sean Kim <dev.seakim@gmail.com>
2018-10-13 11:17:57 -0400

    adding bootstrapCDN, sass module, testBTNs for login/out/currentUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
+
+import logo from "./img/logo.svg";
+import googleSignInLogo from "./img/google_signin_logo.png";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a href="http://localhost:5000/auth/google"><div><img src={googleSignInLogo}/></div></a>
+          <a href="http://localhost:5000/api/logout"><button type="button" class="btn btn-dark">Logout</button></a>
+          <a href="http://localhost:5000/api/current_user"><button type="button" class="btn btn-primary">Current User</button></a>
         </header>
       </div>
     );
