37d60e4bc7af893d8f49f3b9147b8904dab64e51
Sean Kim <dev.seakim@gmail.com>
2018-10-20 19:21:14 -0400

    selected engine - yarn 1.4.0 for heroku

--- config/dev.js
+++ config/dev.js
@@ -4,3 +4,8 @@ module.exports = {
     mongoURI: "mongodb://localhost/jotter",
     cookieKey: "aseflw4ruwaelkjf23pucaspdlmcawjcojp2aSjdfASDFJwliefjasl23jiewuafjsDFklcmioe23ed"
 }
+
+// PROD
+// GOOGLE_CLIENT_ID: 902922861189-ca8clir4dk695puqopeh0ghg04c0lp87.apps.googleusercontent.com
+// GOOGLE_CLIENT_SECRET: WOTgnGWbDUD5kCJHAGWGYAD4
+// MONGO_URI: mongodb://seakim:password4321@ds237563.mlab.com:37563/test-jotter
