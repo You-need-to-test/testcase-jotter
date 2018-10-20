d37d9b546ecebb527c35c41390c86e3134eac57c
ronak <knockronak@gmail.com>
2018-10-20 10:37:59 -0400

    Updating git ignore to have acess to config file

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "861878375459-395qpin09t2vvt43bhma8939ve24gr7n.apps.googleusercontent.com",
+    googleClientSecret: "MpZFe9XBMNs0ru4fZ4exyulY",
+    mongoURI: "mongodb://localhost/jotter",
+    cookieKey: "aseflw4ruwaelkjf23pucaspdlmcawjcojp2aSjdfASDFJwliefjasl23jiewuafjsDFklcmioe23ed"
+}
