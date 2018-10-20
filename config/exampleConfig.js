cbb8a2346515451f923de09e90aa2b499653283e
ronak <knockronak@gmail.com>
2018-10-20 10:01:35 -0400

    changing placement of login button

--- config/exampleConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: "YOUR-GOOGLE-CLIENT-ID",
-    googleClientSecret: "YOUR-GOOGLE-CLIENT-SECRET",
-    mongoURI: "YOUR-MONGO-URL",
-    cookieKey: "aseflw4ruwaelkjf23pucaspdlmcawjcojp2aSjdfASDFJwliefjasl23jiewuafjsDFklcmioe23ed"
-}
