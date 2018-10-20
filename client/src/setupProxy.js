e30011b1be01e863adc9d196088e70bc0fd25a71
ronak <knockronak@gmail.com>
2018-10-20 10:49:55 -0400

    adding dynamic port

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,7 @@
 const proxy = require('http-proxy-middleware')
- 
+const PORT = process.env.PORT || 5000;
+
 module.exports = function(app) {
-  app.use(proxy('/api/*', { target: 'http://localhost:5000/' }))
-  app.use(proxy('/auth/*', { target: 'http://localhost:5000/' }))
+  app.use(proxy('/api/*', { target: 'http://localhost:'+ PORT }))
+  app.use(proxy('/auth/*', { target: 'http://localhost:' + PORT }))
 }
