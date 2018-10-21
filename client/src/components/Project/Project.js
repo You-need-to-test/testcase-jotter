350c46923566bbbbcf8642a0f9a02f56088d153d
ronak <knockronak@gmail.com>
2018-10-21 17:29:52 -0400

    Mock navbar, along with project dropdown field

--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -1,25 +1,51 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import Library from "../Library/Library";
+import Select from 'react-select'
+import {Navbar, NavItem, Row, Col} from 'react-materialize'
 
+
+const options = [
+  { value: 'Project 1', label: 'Project 1' },
+  { value: 'Project 2', label: 'Project 2' },
+  { value: 'Project 3', label: 'Project 3' }
+]
 class Project extends Component {
+
   showCurrentUser() {
     if (!this.props.auth) {
       return null;
     } 
     return (
       <div>
-        <h5>Welcome {this.props.auth.givenName} {this.props.auth.familyName}</h5>
-        <a href="/api/logout"><button type="button" className="btn btn-dark">Logout</button></a>
+        <Row>
+          <Col s={6}>
+            <h5>{this.props.auth.givenName} {this.props.auth.familyName}</h5>
+          </Col>
+          <Col s={6}>
+            <a href="/api/logout"><button type="button" className="btn btn-dark">Logout</button></a>
+          </Col>
+        </Row>
       </div>
     );
   }
   render() {
     return (
       <div>
-        {this.showCurrentUser()}
+        <Navbar brand='TestCase Jotter' className=" cyan accent-4" right>
+          <NavItem >{this.showCurrentUser()}</NavItem>
+        </Navbar>
 
-        <h5>Project</h5>
+        <div>
+          <Row>
+            <Col s={1}>
+              <h4>Projects </h4>
+            </Col>
+            <Col s={3}>
+              <Select options={options} />
+            </Col>
+          </Row>
+        </div>
         <Library />
       </div>
     );
