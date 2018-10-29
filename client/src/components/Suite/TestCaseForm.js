b6ecf19e661a374396f89d2409589d7e6014e5c8
ronak <knockronak@gmail.com>
2018-10-29 00:03:26 -0400

    Adding keyboard event to add and remove test step

--- client/src/components/Suite/TestCaseForm.js
+++ client/src/components/Suite/TestCaseForm.js
@@ -41,7 +41,7 @@ export default class TestCaseForm extends Component {
                     id="dynamic-form">
                 <label htmlFor="dynamic-first">Test Case </label>
                 <Text field="test_case" id="dynamic-first"
-                       // onKeyPress={( ) => formApi.addValue('test_steps', '')}
+                       onKeyDown={e => {if (e.ctrlKey ){formApi.addValue('test_steps', '')}}}
                 />
                 <button
                   onClick={() => formApi.addValue('test_steps', '')}
@@ -49,15 +49,22 @@ export default class TestCaseForm extends Component {
                   className="mb-4 mr-4 btn btn-success">+</button>
 
                 { formApi.values.test_steps && formApi.values.test_steps.map( ( test_step, i ) => (
-                    <div key={`test_step${i}`}>
+                    <div key={`test_step${i}`} className="container">
                       <label htmlFor={`test_step-name-${i}`}>Test Step</label>
                       <Text field={['test_steps', i]} id={`test_step-name-${i}`}
-                            onKeyPress={this.handleKeyPress}
+                            onKeyDown={e => {if (e.altKey )
+                                            {formApi.removeValue('test_steps', i)}
+                                            else if (e.ctrlKey){formApi.addValue('test_steps', '')}}}
+
                       />
                       <button
                         onClick={() => formApi.removeValue('test_steps', i)}
                         type="button"
-                        className="mb-4 btn btn-danger">X</button>
+                        className="mb-4 btn btn-danger">x</button>
+                      <button
+                        onClick={() => formApi.addValue('test_steps', '')}
+                        type="button"
+                        className="mb-4 mr-4 btn btn-success">+</button>
                     </div>
                 ))}
                 <button type="submit" className="mb-4 btn btn-primary">
