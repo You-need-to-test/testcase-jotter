34c67589c4b157a9d018c86f2c500e467b9ee902
ronak <knockronak@gmail.com>
2018-11-01 11:46:20 -0400

    kunle's suggeston

--- server.js
+++ server.js
@@ -1,13 +1,71 @@
+// const express = require("express")
+//     , mongoose = require("mongoose")
+//     , cookieSession = require("cookie-session")
+//     , passport = require("passport")
+//     , bodyParser = require("body-parser");
+// const keys = require("./config/keys");
+// const logger = require('morgan');
+// const path = require("path");
+//
+//
+//
+// /** MODELS */
+// require("./models/User");
+// require("./services/passport");
+//
+// const app = express();
+//
+// app.use(
+//   cookieSession({
+//     maxAge: 10000000,
+//     keys: [keys.cookieKey]
+//   })
+// );
+// app.use(logger('dev'));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// if (typeof process.env.MONGODB_URI !== 'undefined' && process.env.MONGODB_URI.length > 0) {
+// 	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// }
+// else {
+// 	mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+// }
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+//
+// /** ROUTES */
+// require("./routes/authRoutes")(app);
+// require("./routes/projectRoutes")(app);
+// require("./routes/libraryRoutes")(app);
+// require("./routes/suiteRoutes")(app);
+// require("./routes/testcaseRoutes")(app);
+// // require("./routes/teststepRoutes")(app);
+//
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
+//
+// app.get("/project", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> Server now on port ${PORT}!`);
+// });
+//
+//
 const express = require("express")
-    , mongoose = require("mongoose")
-    , cookieSession = require("cookie-session")
-    , passport = require("passport")
-    , bodyParser = require("body-parser");
+  , mongoose = require("mongoose")
+  , cookieSession = require("cookie-session")
+  , passport = require("passport")
+  , bodyParser = require("body-parser");
 const keys = require("./config/keys");
 const logger = require('morgan');
-const path = require("path");
-
-
 
 /** MODELS */
 require("./models/User");
@@ -15,7 +73,7 @@ require("./services/passport");
 
 const app = express();
 
-app.use( 
+app.use(
   cookieSession({
     maxAge: 10000000,
     keys: [keys.cookieKey]
@@ -28,16 +86,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (typeof process.env.MONGODB_URI !== 'undefined' && process.env.MONGODB_URI.length > 0) {
-	mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-}
-else {
-	mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
-}
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 /** ROUTES */
 require("./routes/authRoutes")(app);
 require("./routes/projectRoutes")(app);
@@ -46,16 +94,22 @@ require("./routes/suiteRoutes")(app);
 require("./routes/testcaseRoutes")(app);
 // require("./routes/teststepRoutes")(app);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+if (typeof process.env.MONGODB_URI !== 'undefined' && process.env.MONGODB_URI.length > 0) {
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+}
+else {
+  mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+}
+if(['production'].includes(process.env.NODE_ENV))
+{
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
 
-app.get("/project", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
-
-
