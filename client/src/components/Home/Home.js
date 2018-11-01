4bc770b1124f771f5da91f403dc9559cc93263d5
sk <dev.seakim@gmail.com>
2018-11-01 10:12:26 -0400

    removed path/to/node_modules/@blueprintjs from index / login page touch up

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,21 +1,19 @@
 import React from "react";
 import googleSignInLogo from '../../img/google_signin_logo.png';
 // import LoginPage from './LoginPage'
-import {Row, Col} from 'react-materialize'
+import test_quire from "../../img/test_quire.png";
 
 export default function Home() {
   return (
     <div>
-      <Row>
-        <Col s={4}>
-          {/* <LoginPage/> */}
-          <button><a href="/auth/google"><img src={googleSignInLogo} alt="alt"/></a></button>
-        </Col>
-        <Col s={8}>
-          <img src="http://placeimg.com/1085/910/any/grayscale" alt="Smiley face"/>
-        </Col>
-      </Row>
-      {/* <button><a href="/auth/google"><img src={googleSignInLogo} alt="alt"/></a></button> */}
+      <div className="center" style={{width: "100%", height:"15vh"}}>
+        <img src={test_quire} alt={""} style={{ height: "100px", width: "600px" }} />
+      </div>
+      <img style={{width: "100%", height:"70vh"}} src="http://placeimg.com/1085/910/any/grayscale" alt="Smiley face"/>
+      <div className="center" style={{width: "100%", height:"15vh"}}>
+        <br/>
+        <button><a href="/auth/google"><img src={googleSignInLogo} alt="alt"/></a></button>
+      </div>
     </div>
   );
 }
