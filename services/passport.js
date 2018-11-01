22088cdfc58f446b7fb873ccdac95eb63a4d8cda
ronak <knockronak@gmail.com>
2018-11-01 13:39:08 -0400

    Updating keys based on env

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport")
 		, GoogleStrategy = require("passport-google-oauth20").Strategy
   	,	mongoose = require("mongoose");
-const keys = require("../config/dev");
+const keys = require("../config/keys");
 const User = mongoose.model("users");
 
 passport.serializeUser( (user, done) => {
