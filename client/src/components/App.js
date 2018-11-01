5e0f6209970b46ad9fd087d1dd300dd8e8d852f7
Sean Kim <dev.seakim@gmail.com>
2018-10-31 22:47:01 -0400

    removed materialize npm

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import "materialize-css/dist/css/materialize.min.css";
 import Home from "./Home/Home";
 import Project from "./Project/Project";
 
