84bf0d47d38ad3ea257b1ecf78b1f41f02a2813a
ronak <knockronak@gmail.com>
2018-11-01 10:52:06 -0400

    hardcoding path

--- config/dev.js
+++ config/dev.js
@@ -5,7 +5,7 @@ module.exports = {
     cookieKey: "aseflw4ruwaelkjf23pucaspdlmcawjcojp2aSjdfASDFJwliefjasl23jiewuafjsDFklcmioe23ed"
 }
 
-// PROD
+// // PROD
 // GOOGLE_CLIENT_ID: 902922861189-ca8clir4dk695puqopeh0ghg04c0lp87.apps.googleusercontent.com
 // GOOGLE_CLIENT_SECRET: WOTgnGWbDUD5kCJHAGWGYAD4
 // MONGO_URI: mongodb://seakim:password4321@ds237563.mlab.com:37563/test-jotter
--- server.js
+++ server.js
@@ -47,8 +47,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+app.get("/project", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
+
 
